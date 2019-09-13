9571e8aea9684fab8aa86637f86e2c56de11dfb6
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-09-12 19:51:08 -0700

    Starbot.class
    Implemented base code required to start JDA

---
+++ src/main/java/wenjalan/starbot/Starbot.java
@@ -0,0 +1,62 @@
+package wenjalan.starbot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.util.Scanner;
+
+// the main Starbot class, the entry point for the program
+public class Starbot {
+
+    // program entry point, we require a bot token to start
+    public static void main(String[] args) {
+        // announce that we're starting
+        System.out.println("Starting Starbot 4...");
+
+        // find the discord bot token from args or ask for one
+        String token = null;
+        if (args.length > 0) {
+            token = args[0];
+            System.out.println("Found token from args!");
+        }
+        else {
+            System.out.println("Please enter a bot token:");
+            token = new Scanner(System.in).nextLine();
+        }
+
+        // create a new Starbot
+        new Starbot(token);
+    }
+
+    // Starbot constructor, call with a botToken to create a new Starbot
+    protected Starbot(String botToken) {
+        // initialize JDA
+        JDA jda = getJDA(botToken);
+    }
+
+    // returns an instance of the JDA given a bot token
+    // returns null if an error occurred
+    protected JDA getJDA(String token) {
+        // create a Builder
+        JDABuilder builder = new JDABuilder(token);
+
+        // attach listeners
+        // TODO: Attach said listeners
+
+        // build
+        JDA instance = null;
+        try {
+            instance = builder.build().awaitReady();
+        } catch (LoginException | InterruptedException e) {
+            // if an error occurs, stop the program
+            System.err.println("Encountered an error while trying to build JDA!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // return the instance
+        return instance;
+    }
+
+}
