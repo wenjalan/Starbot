436bef999d8879dbb1c97b60b7d0df3aee003ece
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-09-17 14:04:05 -0700

    AudioEngine
    Added all audio playback commands including
    !play
    !queue
    !clearqueue
    !volume
    !skip
    !playing
    !pause
    !resume
    
    and probably a few I forgot

--- src/main/java/wenjalan/starbot/engine/AudioEngine.java
+++ src/main/java/wenjalan/starbot/engine/AudioEngine.java
@@ -14,14 +14,19 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
+import org.apache.commons.codec.binary.StringUtils;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 // handles all the audio playback capability of Starbot, owns the RadioEngine and MusicEngine
 public class AudioEngine {
@@ -34,6 +39,7 @@ public class AudioEngine {
             @Override
             public void execute(GuildMessageReceivedEvent event) {
                 // if we're not already connected, connect
+                Player p = null;
                 if (!event.getGuild().getAudioManager().isConnected()) {
                     // find the VoiceChannel
                     VoiceChannel channel = event.getMember().getVoiceState().getChannel();
@@ -45,11 +51,187 @@ public class AudioEngine {
                     }
 
                     // connect
-                    Player p = connect(channel);
+                    p = connect(channel);
 
                     // play the query
+                    AudioCommand.playQuery(p, event);
+                }
+                else {
+                    // get the player
+                    p = AudioCommand.getSendHandler(event).player();
+
+                    // play the query
+                    AudioCommand.playQuery(p, event);
+                }
+            }
+        },
+
+        // sends the currently playing track
+        playing {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+
+                // if the handler's not null
+                if (handler != null) {
+                    // get the currently playing track
+                    AudioTrack playing = handler.player().audioPlayer.getPlayingTrack();
+
+                    // if it's not null, print the info
+                    if (playing != null) {
+                        handler.player().sendInfo(playing);
+                    }
+                    else {
+                        event.getChannel().sendMessage("nothing right now").queue();
+                    }
+                }
+            }
+        },
+
+        // pauses the player
+        pause {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // get the SendHandler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+                // if we're playing something, pause it
+                if (handler != null && !handler.player().audioPlayer.isPaused()) {
+                    handler.player().audioPlayer.setPaused(true);
+                }
+            }
+        },
+
+        // resumes the player
+        resume {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // get the SendHandler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+                // if we're paused, unpause
+                if (handler != null && handler.player().audioPlayer.isPaused()) {
+                    handler.player().audioPlayer.setPaused(false);
+                }
+            }
+        },
+
+        // skips the current track
+        skip {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // get handler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+
+                // skip the currently playing track if there is one
+                if (handler != null) {
+                    handler.player().skip();
+                }
+            }
+        },
+
+        // sets the volume
+        volume {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // get the SendHandler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+                // if the player exists, set the volume
+                if (handler != null) {
+                    try {
+                        // find what volume they want
+                        int newVol = Integer.parseInt(CommandEngine.parseArgs(event.getMessage())[1]);
+                        if (newVol > 100 || newVol < 0) {
+                            throw new IllegalArgumentException("volume is from 0 to 100");
+                        }
+
+                        // actually set it
+                        handler.player().audioPlayer.setVolume(newVol);
+                    } catch (IllegalArgumentException e) {
+                        // complain
+                        event.getChannel().sendMessage(e.getMessage()).queue();
+                    }
+                }
+            }
+        },
+
+        // sends the currently queued tracks
+        queue {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // get handler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+
+                // if not null, print the queue
+                if (handler != null) {
+                    // send the queue
+                    event.getChannel().sendMessage(handler.player().getQueueAsString()).queue();
+                }
+            }
+        },
+
+        // removes all tracks from the queue
+        clearqueue {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // handler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+
+                // if not null, remove all queued tracks
+                if (handler != null) {
+                    handler.player().scheduler.queue().clear();
+                }
+            }
+        },
+
+        // seeks to a certain position
+        seek {
+            @Override
+            public void execute(GuildMessageReceivedEvent event) {
+                // get the handler
+                Player.SendHandler handler = AudioCommand.getSendHandler(event);
+                // if the player exists, seek to their desired timestamp
+                if (handler != null && handler.player().audioPlayer.getPlayingTrack() != null) {
+                    // get their desired timestamp, in milliseconds
+                    // format: mm:ss
                     String[] query = CommandEngine.parseArgs(event.getMessage());
-                    p.load(query[1], event.getChannel());
+
+                    // if a timestamp wasn't specified, complain
+                    if (query.length < 2) {
+                        event.getChannel().sendMessage("to what, idiot").queue();
+                        return;
+                    }
+
+                    // split the timestamp into minutes:seconds
+                    // todo: make this more elegant
+                    String[] timestamp;
+                    try {
+                        if (!query[1].contains(":")) {
+                            throw new IllegalArgumentException();
+                        }
+                        timestamp = query[1].split(":");
+                    } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+                        event.getChannel().sendMessage("[min]:[sec]").queue();
+                        return;
+                    }
+
+                    // verify that both are positive numbers
+                    long minutes;
+                    long seconds;
+                    try {
+                        minutes = Integer.parseInt(timestamp[0]);
+                        seconds = Integer.parseInt(timestamp[1]);
+                        if (minutes < 0 || seconds < 0) {
+                            throw new IllegalArgumentException("gimme better numbers");
+                        }
+                    } catch (IllegalArgumentException e) {
+                        event.getChannel().sendMessage("what").queue();
+                        return;
+                    }
+
+                    // calculate the timestamp in milliseconds
+                    long milliseconds = (minutes * 60 * 1000) + (seconds * 1000);
+
+                    // seek to that time
+                    handler.seek(milliseconds);
                 }
             }
         },
@@ -62,6 +244,9 @@ public class AudioEngine {
                 AudioManager manager = event.getGuild().getAudioManager();
                 // if we're connected
                 if (manager.isConnected()) {
+                    // clear the queue
+                    AudioCommand.clearqueue.execute(event);
+
                     // disconnect
                     manager.closeAudioConnection();
                 }
@@ -69,6 +254,25 @@ public class AudioEngine {
         };
 
         public abstract void execute(GuildMessageReceivedEvent event);
+
+        // gets the SendHandler of a guild
+        private static Player.SendHandler getSendHandler(GuildMessageReceivedEvent event) {
+            return (Player.SendHandler) event.getGuild().getAudioManager().getSendingHandler();
+        }
+
+        // plays a query on a given Player
+        private static void playQuery(Player p, GuildMessageReceivedEvent event) {
+            String rawQuery = event.getMessage().getContentRaw();
+            // if there was none
+            if (rawQuery.split(" ").length < 2) {
+                // complain
+                event.getChannel().sendMessage("play what").queue();
+                return;
+            }
+            // remove the !play command from the query
+            rawQuery = rawQuery.substring(6);
+            p.load(rawQuery, event.getChannel());
+        }
 
     }
 
@@ -82,33 +286,33 @@ public class AudioEngine {
         public static final int DEFAULT_PLAYBACK_TIMEOUT = 15 * 60; // 15 minutes
 
         // the AudioPlayerManager
-        protected AudioPlayerManager audioPlayerManager;
+        private AudioPlayerManager audioPlayerManager;
 
         // the AudioPlayer
-        final protected AudioPlayer audioPlayer;
+        final private AudioPlayer audioPlayer;
 
         // the Scheduler
-        final protected Scheduler scheduler;
+        final private Scheduler scheduler;
 
         // the Guild this Player is assigned to
-        protected final Guild guild;
+        private final Guild guild;
 
         // the timeout thread
-        protected Thread timeoutThread;
+        private Thread timeoutThread;
 
         // the SendHandler
-        protected SendHandler sendHandler;
+        private SendHandler sendHandler;
 
         // the last TextChannel we received a command from
-        protected TextChannel lastFeedbackChannel = null;
+        private TextChannel lastFeedbackChannel = null;
 
         // returns the SendHandler for JDA
-        protected AudioSendHandler sendHandler() {
+        private AudioSendHandler sendHandler() {
             return this.sendHandler;
         }
 
         // constructor
-        protected Player(Guild guild) {
+        private Player(Guild guild) {
             this.guild = guild;
 
             // create a manager
@@ -124,22 +328,22 @@ public class AudioEngine {
             this.scheduler = new Scheduler();
 
             // create a SendHandler
-            this.sendHandler = new SendHandler(this.audioPlayer);
+            this.sendHandler = new SendHandler(this);
         }
 
         // the TrackScheduler, handles the adding and removing of tracks from the queue
         protected class Scheduler extends AudioEventAdapter {
 
             // the queue of tracks
-            protected Queue<AudioTrack> queue;
+            private Queue<AudioTrack> queue;
 
             // constructor
-            protected Scheduler() {
+            private Scheduler() {
                 this.queue = new LinkedList<>();
             }
 
             // adds an AudioTrack to the queue
-            protected void queue(AudioTrack track) {
+            private void queue(AudioTrack track) {
                 // if nothing else is playing, start playing
                 if (queue.isEmpty() && audioPlayer.getPlayingTrack() == null) {
                     audioPlayer.playTrack(track);
@@ -189,25 +393,30 @@ public class AudioEngine {
                 lastFeedbackChannel.sendMessage("uhhhhhhhh there was a problem").queue();
             }
 
+            // returns the Queue of tracks
+            public Queue<AudioTrack> queue() {
+                return this.queue;
+            }
+
         }
 
         // the SendHandler, for JDA
         protected class SendHandler implements AudioSendHandler {
 
-            // the AudioPlayer this SendHandler is for
-            protected final AudioPlayer player;
+            // the Player this SendHandler is sending for
+            private Player player;
 
             // the last frame we sent
-            protected AudioFrame lastFrame;
+            private AudioFrame lastFrame;
 
             // constructor
-            protected SendHandler(AudioPlayer p) {
+            private SendHandler(Player p) {
                 this.player = p;
             }
 
             @Override
             public boolean canProvide() {
-                lastFrame = player.provide();
+                lastFrame = player.audioPlayer.provide();
                 return lastFrame != null;
             }
 
@@ -221,12 +430,38 @@ public class AudioEngine {
                 return true;
             }
 
+            // seeks to a position in a Track if it support sit
+            protected void seek(long milliseconds) {
+                // get the playing track
+                AudioTrack t = player.audioPlayer.getPlayingTrack();
+
+                // if it exists and if it's seekable, seek
+                if (t != null && t.isSeekable()) {
+                    t.setPosition(milliseconds);
+                }
+            }
+
+            // returns the AudioPlayer this SendHandler is sending for
+            protected Player player() {
+                return this.player;
+            }
+
         }
 
         // loads a track to play given a String query and a channel to send the results to
-        public void load(String query, final TextChannel feedbackChannel) {
+        protected void load(String query, final TextChannel feedbackChannel) {
             // set the feedback channel so we have somewhere to complain to
             this.lastFeedbackChannel = feedbackChannel;
+
+            // if it's not a url, append the search tag
+            final boolean isSearch;
+            if (!query.startsWith("https")) {
+                query = "ytsearch:" + query;
+                isSearch = true;
+            }
+            else {
+                isSearch = false;
+            }
 
             // load the query
             this.audioPlayerManager.loadItem(query, new AudioLoadResultHandler() {
@@ -238,9 +473,16 @@ public class AudioEngine {
 
                 @Override
                 public void playlistLoaded(AudioPlaylist playlist) {
-                    // queue all the tracks from the playlist
-                    for (AudioTrack track : playlist.getTracks()) {
-                        scheduler.queue(track);
+                    // if it was a search, only add the first track
+                    if (isSearch) {
+                        scheduler.queue(playlist.getTracks().get(0));
+                    }
+                    // otherwise, add all of the tracks
+                    else {
+                        // queue all the tracks from the playlist
+                        for (AudioTrack track : playlist.getTracks()) {
+                            scheduler.queue(track);
+                        }
                     }
                 }
 
@@ -258,6 +500,18 @@ public class AudioEngine {
             });
         }
 
+        // skips the currently playing track
+        public void skip() {
+            // stop the current track
+            audioPlayer.stopTrack();
+            // play the next one if there is one
+            if (!this.scheduler.queue().isEmpty()) {
+                AudioTrack t = scheduler.queue.remove();
+                audioPlayer.startTrack(t, false);
+                sendInfo(t);
+            }
+        }
+
         // sends information about a track into the feedbackChannel
         protected void sendInfo(AudioTrack track) {
             // send some info about the track
@@ -267,6 +521,24 @@ public class AudioEngine {
             lastFeedbackChannel.sendMessage(
                     info.title + " by " + info.author + " (" + minutes + ":" + seconds + ")"
             ).queue();
+        }
+
+        // gets the Queue as a String
+        protected String getQueueAsString() {
+            if (scheduler.queue().isEmpty()) {
+                return "nothing's queued";
+            }
+            else {
+                List<AudioTrack> tracks = new LinkedList<>(scheduler.queue);
+                StringBuilder builder = new StringBuilder();
+                int pos = 1;
+                for (AudioTrack t : tracks) {
+                    AudioTrackInfo info = t.getInfo();
+                    builder.append(pos + ") " + info.title + " by " + info.author + "\n");
+                    pos++;
+                }
+                return builder.toString();
+            }
         }
 
     }
