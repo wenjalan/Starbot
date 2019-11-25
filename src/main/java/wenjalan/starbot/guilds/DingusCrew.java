e2d326809b0f793ecee97426488f6c9a3e02edb3
Alan Wen <alanjameswen@outlook.com>
2019-11-24 18:07:23 -0800

    DingusCrew !play kicking
    if the !play command is used from 8 PM to 4 AM, there's a 1% chance of the user getting kicked
    
    and the !plays command was added for DingusCrew

--- src/main/java/wenjalan/starbot/guilds/DingusCrew.java
+++ src/main/java/wenjalan/starbot/guilds/DingusCrew.java
@@ -3,10 +3,14 @@ package wenjalan.starbot.guilds;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import wenjalan.starbot.engine.CommandEngine;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Random;
 
 // actions specific to The Dingus Crew
 public class DingusCrew {
@@ -25,6 +29,10 @@ public class DingusCrew {
         public static final long GULLIBLE_RETARD_ID_LONG = 289563937299628032L; // gullible retard
 
     }
+
+    // Fields
+    // the number of times the !play command has been used since startup
+    protected static int totalPlays = 0;
 
     // Command Enum
     public enum DingusCrewCommand {
@@ -46,14 +54,11 @@ public class DingusCrew {
                 }
 
                 // kick him
-                e.getGuild().kick(justin).reason("you don't have the n word pass").queue();
-
-                // reinvite him
-                String invite = e.getChannel().createInvite().setMaxUses(1).complete().getUrl();
-                justin.getUser().openPrivateChannel().complete().sendMessage(invite).queue();
+                kickAndReinvite(e.getGuild(), justin, "you don't have the n word pass");
             }
         },
 
+        // wife: kick jose and reinvites him
         wife {
             @Override
             public void execute(GuildMessageReceivedEvent e) {
@@ -65,16 +70,47 @@ public class DingusCrew {
                 // if both check out, kick and reinvite jose
                 Member jose = e.getGuild().getMemberById(Constants.JOSE_ID_LONG);
                 if (jose == null) {
-                    e.getChannel().sendMessage("couldn't find justin").queue();
+                    e.getChannel().sendMessage("couldn't find jose").queue();
                     return;
                 }
 
-                // kick him
-                e.getGuild().kick(jose).reason("you don't have the w word pass").queue();
+                // kick and reinvite him
+                DingusCrewCommand.kickAndReinvite(e.getGuild(), jose, "you don't have the w word pass");
+            }
+        },
 
-                // reinvite him
-                String invite = e.getChannel().createInvite().setMaxUses(1).complete().getUrl();
-                jose.getUser().openPrivateChannel().complete().sendMessage(invite).queue();
+        // prints out the number of times !play has been used since startup
+        plays {
+            @Override
+            public void execute(GuildMessageReceivedEvent e) {
+                e.getChannel().sendMessage("!play has been used " + totalPlays + " times").queue();
+            }
+        },
+
+        // play: additional play command kicks people
+        // - when its from 8 PM to 4 AM
+        // - 1% chance
+        play {
+            @Override
+            public void execute(GuildMessageReceivedEvent e) {
+                // sout
+                System.out.println("caught !play command in dingus crew");
+                // increment totalPlays
+                totalPlays++;
+                // check the time
+                ZonedDateTime now = ZonedDateTime.now(ZoneId.of(ZoneId.SHORT_IDS.get("PST")));
+
+                // if it's 8 to 4
+                if (now.getHour() > 17 || now.getHour() < 3) {
+                    // roll a die
+                    int roll = new Random().nextInt(100);
+                    // if the roll was 0, kick the author
+                    if (roll == 0) {
+                        // announce their kicking
+                        e.getChannel().sendMessage(e.getMember().getAsMention() + " won the lottery on !play #" + totalPlays).queue();
+                        kickAndReinvite(e.getGuild(), e.getMember(), "won the !play lottery");
+                    }
+                }
             }
         };
 
@@ -103,6 +139,15 @@ public class DingusCrew {
             return true;
         }
 
+        // kicks and reinvites a user
+        protected static void kickAndReinvite(Guild g, Member m, String reason) {
+            // kick them
+            g.kick(m).reason(reason).queue();
+            // reinvite them
+            String invite = g.getDefaultChannel().createInvite().setMaxUses(1).complete().getUrl();
+            m.getUser().openPrivateChannel().complete().sendMessage(invite).queue();
+        }
+
     }
 
     // Listener
@@ -113,6 +158,11 @@ public class DingusCrew {
         public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
             // if the guild wasn't Dingus Crew, return
             if (e.getGuild().getIdLong() != Constants.DINGUS_CREW_GUILD_ID_LONG) {
+                return;
+            }
+
+            // if it was a bot, ignore it
+            if (e.getAuthor().isBot()) {
                 return;
             }
 
