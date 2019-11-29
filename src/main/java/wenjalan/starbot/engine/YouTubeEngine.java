b70f9b38053603412087eb0e85a7c17d6d40bb09
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-11-28 22:59:30 -0800

    YouTubeDataEngine setup

---
+++ src/main/java/wenjalan/starbot/engine/YouTubeEngine.java
@@ -0,0 +1,16 @@
+package wenjalan.starbot.engine;
+
+// handles all the YouTube Data API stuff
+public class YouTubeEngine {
+
+    // the filepath of the text file with the API key
+    private static final String API_KEY_PATH = "keys/google.key";
+
+    // returns a recommended video URL for a given video
+    public static String getRecommendationFor(String url) {
+
+    }
+
+    // sets up the YouTube Data API for use
+
+}
