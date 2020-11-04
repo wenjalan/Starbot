07cd85018ac5f313781bc15397bdf8f38eb43258
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2020-11-03 23:48:35 -0800

    Help Command

--- src/main/java/wenjalan/starbot/engine/CommandEngine.java
+++ src/main/java/wenjalan/starbot/engine/CommandEngine.java
@@ -1,13 +1,99 @@
 package wenjalan.starbot.engine;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import wenjalan.starbot.engine.command.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // handles all command-related functionality
 public class CommandEngine {
+
+    // the help command
+    // defined here because it needs access to all activated commands
+    public class HelpCommand implements Command {
+        @Override
+        public String getName() {
+            return "help";
+        }
+
+        @Override
+        public String getDescription() {
+            return "Provides information on commands and their usages";
+        }
+
+        @Override
+        public String getUsage() {
+            return "!help <command>";
+        }
+
+        @Override
+        public boolean isGuildCommand() {
+            return true;
+        }
+
+        @Override
+        public boolean isDmCommand() {
+            return true;
+        }
+
+        @Override
+        public boolean isAdminCommand() {
+            return false;
+        }
+
+        @Override
+        public void run(Message msg) {
+            // get args and channel
+            String[] args = msg.getContentRaw().split("\\s+");
+            MessageChannel channel = msg.getChannel();
+            boolean isPrivate = msg.getChannelType().equals(ChannelType.PRIVATE);
+
+            // if no args, send list of commands
+            if (args.length <= 1) {
+                // get commands that are either guild or dm commands
+                String commandsList = commands.stream().filter(command -> {
+                    if (isPrivate) {
+                        return command.isDmCommand();
+                    }
+                    else {
+                        return command.isGuildCommand();
+                    }
+                }).map(Command::getName).collect(Collectors.joining("\n"));
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setColor(Color.GREEN);
+                embed.setTitle((isPrivate ? "DM" : "Server") + " Commands");
+                embed.setDescription("Available commands for Starbot 5.0");
+                embed.addField("Page 1 of 1", commandsList, false);
+                embed.setFooter("For specific help, use !help <command>");
+                channel.sendMessage(embed.build()).queue();
+            }
+            // otherwise, send information about the specific command
+            else {
+                // get the right command
+                String query = args[1];
+                for (Command command : commands) {
+                    if (command.getName().equalsIgnoreCase(query)) {
+                        // String commandInfo = "**!" + command.getName() + "**\n" + command.getDescription() + "\n`" + command.getUsage() + "`";
+                        EmbedBuilder embed = new EmbedBuilder();
+                        embed.setColor(Color.GREEN);
+                        embed.setTitle("!" + command.getName());
+                        embed.setDescription(command.getDescription());
+                        embed.addField("Usage", command.getUsage(), true);
+                        channel.sendMessage(embed.build()).queue();
+                        return;
+                    }
+                }
+                // report no command found
+                channel.sendMessage(query + " is not a valid command").queue();
+            }
+        }
+    }
 
     // singleton
     private static CommandEngine instance = null;
@@ -16,7 +102,7 @@ public class CommandEngine {
     public static final String COMMAND_PREFIX = "!";
 
     // a list of executable Commands
-    private List<Command> commands;
+    private final List<Command> commands;
 
     // singleton constructor
     private CommandEngine() {
@@ -31,6 +117,7 @@ public class CommandEngine {
         // add all commands to the list
         // ! this is where all commands must be registered !
         ArrayList<Command> commands = new ArrayList<>();
+        commands.add(new HelpCommand());
         commands.add(new VersionCommand());
         commands.add(new InviteCommand());
         commands.add(new ReloadCommand());
