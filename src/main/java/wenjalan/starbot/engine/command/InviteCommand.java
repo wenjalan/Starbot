d63cd2aaf3e33bc738b96ecb13d8ecddb3dab7ef
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2020-11-17 20:44:03 -0800

    Invite Command Bugfix

--- src/main/java/wenjalan/starbot/engine/command/InviteCommand.java
+++ src/main/java/wenjalan/starbot/engine/command/InviteCommand.java
@@ -2,6 +2,8 @@ package wenjalan.starbot.engine.command;
 
 import net.dv8tion.jda.api.entities.Message;
 
+import static wenjalan.starbot.Starbot.DEFAULT_PERMISSIONS;
+
 // responds with a link to invite Starbot to a server
 public class InviteCommand implements Command {
 
@@ -38,7 +40,7 @@ public class InviteCommand implements Command {
     @Override
     public void run(Message msg) {
         // get and send the url
-        String url = msg.getJDA().getInviteUrl();
+        String url = msg.getJDA().getInviteUrl(DEFAULT_PERMISSIONS);
         msg.getChannel().sendMessage(url).queue();
     }
 }
